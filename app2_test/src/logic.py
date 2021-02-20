8b3db83389f53494dc06a0f6a670cf9778f5789e
Hench89 <hchristoffersen100@gmail.com>
2021-02-20 16:46:05 +0100

    added make test

--- app2_test/src/logic.py
+++ app2_test/src/logic.py
@@ -1,3 +1,7 @@
 def my_function(n1, n2, n3):
-    result = int(n1) * int(n2) - int(n3)
-    return str(result)
+
+    try:
+        result = n1*n2-n3
+        return 1, result
+    except:
+        return 0, 0
--- app2_test/src/routes.py
+++ app2_test/src/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from src.logic import my_function
+from logic import my_function
 
 app = Flask(__name__)
 
@@ -9,7 +9,16 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    n1 = request.form['number1']
-    n2 = request.form['number2']
-    n3 = request.form['number3']
-    return my_function(n1, n2, n3)
+
+    try:
+        
+        n1 = int(request.form['number1'])
+        n2 = int(request.form['number2'])
+        n3 = int(request.form['number3'])
+        status_code, my_value = my_function(n1, n2, n3)
+        
+        if (status_code == 0):
+            return "error in inputs"
+        return str(my_value)
+    except:
+        return "error"
